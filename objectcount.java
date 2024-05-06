2ba16ec8e54a79e84bcb9bb9ade1e75513abdc7c
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2024-05-06 22:42:35 +0530

    Update objectcount.java

--- objectcount.java
+++ objectcount.java
@@ -1,13 +1,11 @@
-import java.util.List;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class objectCount {
-	public static void main(String[] args) {  
+	public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\ravip\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://google.com/mail/help");
-        List<WebElement> allElements = driver.findElements(By.tagName("p"));
-        System.out.println("Total number of objects on the page: " + allElements.size());
+        System.out.println("Total number of objects on the page: " + driver.findElements(By.tagName("p")).size());
         driver.quit();
     }
 }
