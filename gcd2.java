b352824c87d46d7b1e1d9f34062780bcd3b34996
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2024-05-06 18:57:01 +0530

    Update gcd2.java

--- gcd2.java
+++ gcd2.java
@@ -1,32 +1,18 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import java.util.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
-public class Gcd2 {
-    private WebDriver driver;
 
-    @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\annex\\\\Desktop\\\\chromedriver\\\\chromedriver_win32 -new\\\\chromedriver.exe");
-        driver = new ChromeDriver();
-    }
-
-    @Test
-    public void testGcd() {
-        driver.get("file:///C:/GCD.html");
-        driver.manage().window().maximize();
-        driver.findElement(By.name("n1")).sendKeys("5");
-        driver.findElement(By.name("n2")).sendKeys("6");
-        driver.findElement(By.cssSelector("input[type='button']")).click();
-        String result = driver.findElement(By.name("result")).getAttribute("value");
-        System.out.println("the gcd is : " + result);
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+public class gcd {
+	public static void main(String[] args) {
+		 System.setProperty("webdriver.chrome.driver","C:\\Users\\ravip\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+	        ChromeDriver driver = new ChromeDriver();
+		driver.get("C:\\Users\\ravip\\Downloads\\ex11.html");
+		List<WebElement> elements = driver.findElements(By.tagName("input"));
+		elements.get(0).sendKeys("235");
+		elements.get(1).sendKeys("15");
+		elements.get(2).click();
+		String result = elements.get(3).getAttribute("value");
+		System.out.println("The GCD is: "+result);
+	}
 }
