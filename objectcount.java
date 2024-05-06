4c31f0c2d795a26015e5ac9ad2cf77f22c152538
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2024-05-06 22:39:28 +0530

    Update objectcount.java

--- objectcount.java
+++ objectcount.java
@@ -1,19 +1,13 @@
+import java.util.List;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-public class objectcount {
-  
-  public static void main(String[] args) {
-     System.setProperty("webdriver.chrome.driver", "C:\\Users\\annex\\Desktop\\chromedriver\\chromedriver_win32 -new\\chromedriver.exe");
-     WebDriver driver = new ChromeDriver(); 
-     driver.get("http://www.google.com");
-     driver.manage().window().maximize(); 
-     // Find the <p> element on the page
-     WebElement paragraph = driver.findElement(By.tagName("p"));
-     System.out.println("The number of <p> elements present are: " + driver.findElements(By.tagName("p")).size());
-     //System.out.println("Found paragraph: " + paragraph.getText()); // To verify that the correct paragraph was found
-     
-     driver.quit();
-  }
+public class objectCount {
+	public static void main(String[] args) {  
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\ravip\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://google.com/mail/help");
+        List<WebElement> allElements = driver.findElements(By.tagName("p"));
+        System.out.println("Total number of objects on the page: " + allElements.size());
+        driver.quit();
+    }
 }
-  
