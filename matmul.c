663c6c7c3869f7d349795886a3b800cfd9912dd3
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2024-05-06 23:34:07 +0530

    Update matmul.c

--- matmul.c
+++ matmul.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 #include<conio.h>
 int a[10][10],b[10][10],c[10][10];
-int m,n,p,q;
+int m,n,p,q,flag=0;
+char x[10];
+int isDot(char *x,char c)
+{
+while(*x)
+{
+ if (*x==c)
+   return 1;
+   x++;}
+ return 0;
+}
 void main()
 {
   int i,j,k;
@@ -18,17 +28,39 @@ void main()
    {
    for (j=0;j<n;j++)
    {
-    scanf("%d",&a[i][j]);
+    scanf("%s",&x);
+    if(isDot(x,'.'))
+    {
+     printf("TestCase:2-Failed!!! Non Integer value are presented");
+     i=m;
+     flag=1;
+     break;
+    }
+    else
+    a[i][j]=atoi(x);
    }
   }
+  if(flag!=1)
+  {
   printf("\nEnter Matrix B elements \n");
      for(i=0;i<p;i++)
    {
    for (j=0;j<q;j++)
    {
-    scanf("%d",&b[i][j]);
+     scanf("%s",&x);
+    if(isDot(x,'.'))
+    {
+     printf("TestCase:2-Failed!!! Non Integer value are presented");
+     i=p;
+     flag=1;
+     break;
+    }
+    else
+    b[i][j]=atoi(x);
    }
   }
+  if(flag!=1)
+  {
   printf("Matrix Multiplication:\n");
   for(i=0;i<m;i++)
   {
@@ -43,6 +75,8 @@ void main()
   }
   printf("\n");
  }
+}
+}
 }
 else
 {
