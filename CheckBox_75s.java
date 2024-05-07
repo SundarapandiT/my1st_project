af93cf841a8912573bad49c324c7c54b475f42c8
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2024-05-07 07:58:15 +0530

    Update CheckBox_75s.java

--- CheckBox_75s.java
+++ CheckBox_75s.java
@@ -1,37 +1,25 @@
-
-import java.util.List;
-import java.util.concurrent.TimeUnit; 
-import org.openqa.selenium.By;
+import java.util.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class CheckBox_75s {
+public class checkBox {
 public static void main(String[] args) {
 
-System.setProperty("webdriver.chrome.driver","C:\\Users\\annex\\Desktop\\chromedriver\\chromedriver_win32 -new\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\ravip\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 ChromeDriver driver = new ChromeDriver();
- driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
- driver.manage().window().maximize();
- driver.get("C:\\New.html");
- List<org.openqa.selenium.WebElement>CheckBoxes =driver.findElements(By.xpath("//input[@type='checkbox']"));
+ driver.get("C:\\Users\\ravip\\Downloads\\07.html");
+ List<WebElement>CheckBoxes =driver.findElements(By.xpath("//input[@type='checkbox']"));
  System.out.println("Number of Check boxes : "+Integer.toString(CheckBoxes.size()));
-for(int i=0; i<CheckBoxes.size(); i=i+4)
-{
-CheckBoxes.get(i).click();
-}
+CheckBoxes.get(0).click();
 int checkedCount=0, uncheckedCount=0;
- for(int i=0; i<CheckBoxes.size(); i++)
-{
-System.out.println(i+" checkbox is selected "+CheckBoxes.get(i).isSelected()); 
-if(CheckBoxes.get(i).isSelected())
-{
-checkedCount++;
-}
-else
-{ 
-uncheckedCount++;
-}
-}
+	if(CheckBoxes.get(0).isSelected())
+	{
+		checkedCount++;
+	}
+	else
+	{ 	
+		uncheckedCount++;
+	}
 System.out.println("number of selected checkbox: "+checkedCount); 
 System.out.println("number of unselected checkbox: "+uncheckedCount);
 }
 }
-
